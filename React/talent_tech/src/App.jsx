ef54184dee752fa1bca1f08da58e1ed2d93f57d9
anthony-xu <caterva17@hotmail.com>
2025-05-07 20:39:16 -0300

    [LearnFW][React]=> Install React-Router

--- React/talent_tech/src/App.jsx
+++ React/talent_tech/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/header/Header'
-
+import { useState } from "react";
+import "./App.css";
+import Navegacion from "./pages/navbar/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  const image= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr7bgS6YAbZxPIHqokEhHKvEaa0uosSzhLQQ&s"
+  const image =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr7bgS6YAbZxPIHqokEhHKvEaa0uosSzhLQQ&s";
 
   return (
     <>
- 
       <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
