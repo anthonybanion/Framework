3dfef1c045aae1b02438329163842d76c1fc37b0
anthony-xu <caterva17@hotmail.com>
2025-05-12 11:52:20 -0300

    [LearnFW][React]=> Refactory, re-fixing the changes

--- React/talent_tech/src/App.jsx
+++ React/talent_tech/src/App.jsx
@@ -1,23 +1,17 @@
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
     <>
       <Router>
-        
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
-          
         </Routes>
       </Router>
-     
     </>
   );
 }
