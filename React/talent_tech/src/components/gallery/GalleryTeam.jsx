3db99854df74bd3ce56603533d3d485cb2408e92
anthony-xu <caterva17@hotmail.com>
2025-05-18 21:40:03 -0300

    [LearnFW][React]=> Refactory react components; See #69

--- React/talent_tech/src/components/gallery/GalleryTeam.jsx
+++ React/talent_tech/src/components/gallery/GalleryTeam.jsx
@@ -1,6 +1,23 @@
+// ==========================================
+//
+// Displays a gallery of team members.
+//
+// File: GalleryTeam.jsx
+// Author: Anthony Bañon
+// Created: 2025-05-18
+// Last Updated: 2025-05-18
+// ==========================================
+
 import "./galleryTeam.css";
 
 function GalleryTeam({ team }) {
+  /*
+   * This function renders a section containing a gallery of team members.
+   *
+   * @param {Array} team - The list of team members.
+   * @returns {JSX.Element} The rendered gallery section.
+   */
+
   return (
     <section className="mySection">
       {team.map((person, index) => (
